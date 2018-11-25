bde4fb317a13af781bd12c6e3a1ed003aedebd23
robert neuzil <robertneuzil@gmail.com>
2018-11-25 00:22:55 -0500

    django variables and query set in template

--- pollme/polls/views.py
+++ pollme/polls/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from . models import Poll
 
 
 def polls_list(request):
+	'''
+	renders polls_list template along with the currently available polls, in context
 
+	'''
+
+	polls = Poll.objects.all()
 	context = {
-
+	
+	"polls": polls
+	
 	}
 	return render(request, "polls/polls_list.html", context)
 # Create your views here.
